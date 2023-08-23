0761a1307b564eadcc75b9c2b535c5527ccdc5f9
Zahid Hasan <zahidcse98@gmail.com>
2023-08-24 00:24:00 +0600

    project added

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+  const baseURL = 'http://localhost:5000/api/items';
+  const fetchItems = async () => {
+    const response = await axios.get(baseURL);
+    const {data} = response;
+    console.log(data);
+    setItems(data);
+  };
+
+  const postItems = async (name, description) => {
+    const response = await axios.post(baseURL, {name , description})
+    console.log(response)
+  }
+
+  const deleteItem = async (id) => {
+    const deletedItem = await axios.delete(`${baseURL}/${id}`)
+    console.log(deletedItem);
+    fetchItems();
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    postItems(name, description)
+    setName('');
+    setDescription('');
+    fetchItems();
+    console.log('submit request');
+  };
+
+
+  return (
+    <div className="App">
+      <h1>CRUD App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" value={name || ''} onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="Description" value={description || ''} onChange={(e) => setDescription(e.target.value)} />
+        <button type="submit">Add Item</button>
+      </form>
+      <ul>
+        {items.map(item => (
+          <li key={item?._id}>
+            <h3>{item?.name}</h3>
+            <p>{item?.description}</p>
+            <button onClick={() => deleteItem(item._id)}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
