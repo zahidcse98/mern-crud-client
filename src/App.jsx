5f6d678198fc9e6bf64bb25defbb2a5797e48c2a
Zahid Hasan <zahidcse98@gmail.com>
2023-08-25 02:55:47 +0600

    project update 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,48 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [items, setItems] = useState([]);
+  //const [items, setItems] = useState([]);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [serverName, setServerName] = useState('');
 
   
   useEffect(() => {
-    fetchItems();
+    //fetchItems();
     getServerName();
   }, []);
 
-  const baseURL = 'http://localhost:5000/api/items';
-  const fetchItems = async () => {
-    const response = await axios.get(baseURL);
-    const {data} = response;
-    console.log(data);
-    setItems(data);
-  };
+ //const baseURL = 'http://localhost:5000/api/items';
+  // const fetchItems = async () => {
+  //   const response = await axios.get("http://34.148.139.225/api/items");
+  //   const {data} = response;
+  //   console.log(data);
+  //   setItems(data);
+  // };
+
+
   const getServerName = async () => {
-    const response = await axios.get(`${baseURL}/server`)
+    const response = await axios.get("http://34.148.139.225/api/items")
     console.log(response.data);
     setServerName(response.data)
   }
-  const postItems = async (name, description) => {
-    const response = await axios.post(baseURL, {name , description})
-    console.log(response)
-  }
+  // const postItems = async (name, description) => {
+  //   const response = await axios.post(baseURL, {name , description})
+  //   console.log(response)
+  // }
 
-  const deleteItem = async (id) => {
-    const deletedItem = await axios.delete(`${baseURL}/${id}`)
-    console.log(deletedItem);
-    fetchItems();
-  }
+  // const deleteItem = async (id) => {
+  //   const deletedItem = await axios.delete(`${baseURL}/${id}`)
+  //   console.log(deletedItem);
+  //   fetchItems();
+  // }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    postItems(name, description)
+   // postItems(name, description)
     setName('');
     setDescription('');
-    fetchItems();
+    //fetchItems();
     console.log('submit request');
   };
 
@@ -55,13 +57,16 @@ function App() {
         <button type="submit">Add Item</button>
       </form>
       <ul>
+{/*         
         {items.map(item => (
           <li key={item?._id}>
             <h3>{item?.name}</h3>
             <p>{item?.description}</p>
-            <button onClick={() => deleteItem(item._id)}>X</button>
-          </li>
-        ))}
+            {/* //<button onClick={() => deleteItem(item._id)}>X</button> */}
+          {/* </li> */}
+        {/* ))}  */}
+        {/* */} 
+
       </ul>
     </div>
   );
